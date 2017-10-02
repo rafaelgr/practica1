61881a1c5e70b2aeccfefee82748ef130a3316b5
Rafael Garcia <rafa@myariadna.com>
2017-10-02 11:15:20 +0200

    commit inicial

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+var jsonParser = bodyParser.json()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// using cors for cross class
+app.use(cors());
+
+
+
+
+app.use('/api/vicent', require('./lib/vicent/vicent.controller'));
+app.listen(8099);
+console.log('Estoy escuichando en el localhost:8099');
